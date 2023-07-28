22aac42bfce64a65682731038e1d5b6d1ba2d0c4
Yanbel0706 <133171485+Yanbel0706@users.noreply.github.com>
2023-07-28 01:04:11 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,7 +5,7 @@
  * list_len - returns the number of elements in a linked list
  * @h: pointer to the t_list list
  *
- * Return: number of elements in list t 
+ * Return: number of elements in list h 
  */
 size_t list_len(const list_t *h)
 {
