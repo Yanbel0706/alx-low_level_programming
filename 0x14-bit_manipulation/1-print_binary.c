14fa6202e2a3e27370b0989ff00c886607952f8c
yanbel0706 <yanis.belk.edu@gmail.com>
2023-10-26 21:28:42 +0100

    another try

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,43 +1,49 @@
 #include "main.h"
+
 /**
- ** _pow - func calculates (base ^ power)
- ** @base: base of the exponent
- ** @power: power of the exponent
- ** Return: value of (base ^ power)
- **/
+ *  * _pow - func calculates (base ^ power)
+ *   * @base: base of the exponent
+ *    * @power: power of the exponent
+ *     *
+ *      * Return: value of (base ^ power)
+ *       */
 unsigned long int _pow(unsigned int base, unsigned int power)
 {
-unsigned long int number;
-unsigned int c;
-number = 1;
-for (c = 1; c <= power; c++)
-  {	
-  number *= base; 
-  } 
-  
-return (number);
+		unsigned long int num;
+			unsigned int a;
+
+				num = 1;
+					for (a = 1; a <= power; a++)
+								num *= base;
+						return (num);
 }
 
 /**
- ** print_binary - prints a number in binary notation
- ** @n: number to print
- ** Return: void
- **/
+ *  * print_binary - prints a number in binary notation
+ *   * @n: number to print
+ *    *
+ *     * Return: void
+ *      */
 void print_binary(unsigned long int n)
 {
-unsigned long int diviseur, chk;
-char flg;
-flg = 0;
-diviseur = _pow(2, sizeof(unsigned long int) * 8 - 1);
-while (diviseur != 0)
-    {
-	chk = n & diviseur;										    if (chk == diviseur)
-	{	flg = 1;
-		_putchar('1');}
-													    else if (flg == 1 || diviseur == 1)
-									    				    {
-														    _putchar('0'); 								
-	    diviseur >>= 1;    	
-													    }    
-    }
+		unsigned long int divisor, check;
+			char flag;
+
+				flag = 0;
+					divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+						while (divisor != 0)
+								{
+											check = n & divisor;
+													if (check == divisor)
+																{
+																				flag = 1;
+																							_putchar('1');
+																									}
+															else if (flag == 1 || divisor == 1)
+																		{
+																						_putchar('0');
+																								}
+																	divisor >>= 1;
+																		}
 }
+
