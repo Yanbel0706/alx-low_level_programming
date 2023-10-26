0de33122762eccbd452f19a28a0f3bdb4211d0de
yanbel0706 <yanis.belk.edu@gmail.com>
2023-10-26 23:04:32 +0100

    over

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,20 +1,19 @@
 #include"main.h"
 
 /**
- *  * get_bit - program returning the value of a bit at a given index.
- *   * @n: number to check the bit 
- *    * @index: index to check  bit
- *     *
- *      * Return: value of the bit, or if get error return -1
- *       */
+ ** get_bit - program returning the value of a bit at a given index.
+ ** @n: number to check the bit
+ ** @index: index to check  bit
+ ** Return: value of the bit, or if get error return -1
+ **/
 int get_bit(unsigned long int n, unsigned int index)
 {
 unsigned long int diviseur, chk;
-   if (index > (sizeof(unsigned long int) * 8 - 1))
-	    return (-1);
-   diviseur = 1 << index;
-   chk = n & diviseur;
-   if (chk == diviseur)
-	    return (1);
-   return (0);
+if (index > (sizeof(unsigned long int) * 8 - 1))
+return (-1);
+diviseur = 1 << index;
+chk = n & diviseur;
+if (chk == diviseur)
+return (1);
+return (0);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *  * clear_bit - This program set the value of a bit to 0 at a known index
- *   * @n: parameter
- *    * @index: index
- *     * Return: 1 if success, -1 on occured any error
- *      */
+ ** clear_bit - This program set the value of a bit to 0 at a known index
+ ** @n: parameter
+ ** @index: index
+ ** Return: 1 if success, -1 on occured any error
+ **/
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-  if (index > sizeof(n) * 8)
-     return (-1);
-  *n &= ~(1 << index);
-  return (1);
+if (index > sizeof(n) * 8)
+return (-1);
+*n &= ~(1 << index);
+return (1);
 }
