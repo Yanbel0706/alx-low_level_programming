327ef764a75d7735c5a91461700fd686e62574dc
yanbel0706 <yanis.belk.edu@gmail.com>
2023-10-26 22:10:28 +0100

    last try

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,16 +7,14 @@
  ** Return: value of (base ^ power)
  **/
 unsigned long int powerfunc(unsigned int base, unsigned int power)
-{
-		unsigned long int number;
-			unsigned int count;
-
-				number = 1;
-					for (count = 1; count <= power; count++)
-								number *= base;
-						return (number);
+{ 
+unsigned long int number;
+unsigned int count;
+number = 1;
+for (count = 1; count <= power; count++)
+number *= base;
+return (number);
 }
-
 /**
  ** print_binary - prints a number in binary notation
  ** @n: number to print
@@ -24,24 +22,18 @@ unsigned long int powerfunc(unsigned int base, unsigned int power)
  **/
 void print_binary(unsigned long int n)
 {
-		unsigned long int div, check;
-			char carac;
-
-				carac = 0;
-					div = powerfunc(2, sizeof(unsigned long int) * 8 - 1);
-						while (div != 0)
-								{
-											check = n & div;
-													if (check == div)
-																{
-																				carac = 1;
-																							_putchar('1');
-																									}
-															else if (carac == 1 || div == 1)
-																		{
-																						_putchar('0');
-																								}
-																	div >>= 1;
-																		}
+unsigned long int div, check;
+char carac;
+carac = 0;
+div = powerfunc(2, sizeof(unsigned long int) * 8 - 1);
+while (div != 0)
+{
+   check = n & div;											if (check == div)
+{
+      carac = 1;											     _putchar('1');											}
+else if (carac == 1 || div == 1)
+{	
+     _putchar('0');}
+     div >>= 1;
 }
-
+}
