d7ce30833633bcc7e024a7b0c1906192c63f373c
yanbel0706 <yanis.belk.edu@gmail.com>
2023-10-26 20:55:16 +0100

    last commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
 /**
- *  * _power - func calculates (base ^ power)
- *   * @base: base of the exponent
- *    * @power: power of the exponent
- *     *
- *      * Return: value of (base ^ power)
- *       */
-unsigned long int _power(unsigned int base, unsigned int power)
+ ** _pow - func calculates (base ^ power)
+ ** @base: base of the exponent
+ ** @power: power of the exponent
+ ** Return: value of (base ^ power)
+ **/
+unsigned long int _pow(unsigned int base, unsigned int power)
 {
 unsigned long int number;
 unsigned int c;
@@ -17,24 +15,25 @@ for (c = 1; c <= power; c++)
   return (number);
 }
 /**
- *  * print_binary - prints a number in binary notation
- *   * @n: number to print
- *    *
- *     * Return: void
- *      */
+ ** print_binary - prints a number in binary notation
+ ** @n: number to print
+ ** Return: void
+ **/
 void print_binary(unsigned long int n)
 {
 unsigned long int diviseur, chk;
 char flg;
 flg = 0;
-diviseur = _power(2, sizeof(unsigned long int) * 8 - 1);
+diviseur = _pow(2, sizeof(unsigned long int) * 8 - 1);
 while (diviseur != 0)
     {
 	chk = n & diviseur;										    if (chk == diviseur)
 	{	flg = 1;
 		_putchar('1');}
 													    else if (flg == 1 || diviseur == 1)
-									    				    {_putchar('0'); 
-														    diviseur >>= 1;    	}
+									    				    {
+														    _putchar('0'); 								
+	    diviseur >>= 1;    	
+													    }    
     }
 }
