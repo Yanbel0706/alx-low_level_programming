797417091e30f200c73ea1531b77a18fc48529a2
yanbel0706 <yanis.belk.edu@gmail.com>
2023-10-26 22:36:55 +0100

    another

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,16 +1,25 @@
-#include "main.h"                                                                                       /**
-**binary_to_uint - function converting a binary number to an unsigned int.                              **@b: pointer to a string which contains a binary number
-*Return: unsigned int with decimal value r or to 0 if error 
-**/            
-unsigned int binary_to_uint(const char *b) 
+#include "main.h"
+/*
+**binary_to_uint - function converting a binary number to an unsigned int.
+**@b: pointer to a string which contains a binary number
+**Return: unsigned int with decimal value r or to 0 if error
+**/
+unsigned int binary_to_uint(const char *b)
 {
-int c;                                                                                                  unsigned int number;                                                                                    number = 0;                                                                                             if (!b) 
+int c;
+unsigned int number;
+number = 0;
+if (!b)
 return (0);
-for (c = 0; b[c] != '\0'; c++)	
-{                                                                                                     
-  if(b[c] != '0' && b[c] != '1')     
-	  return (0);
+for (c = 0; b[c] != '\0'; c++)
+{
+if (b[c] != '0' && b[c] != '1')
+return (0);
 }
-for (c = 0; b[c] != '\0'; c++){                                                                                number <<= 1;
-if (b[c] == '1')   
-number += 1;}                                                                                           return(number);                                                                                         }                                    
+for (c = 0; b[c] != '\0'; c++)
+{
+	number <<= 1;
+if (b[c] == '1')
+number += 1; }
+return (number);
+}
